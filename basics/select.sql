a8e7938b3ceeedbc9735763de87730ea9cae8436
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2020-05-13 17:08:18 -0700

    Se continua trabajando con select

--- basics/select.sql
+++ basics/select.sql
@@ -17,3 +17,35 @@ SELECT DISTINCT [FieldName] FROM [Table];
 --WHERE Trae solo la regla de lo que contenga el where
 SELECT * FROM [TableName] WHERE [FieldName] = '[MyCondition]'
 --Ejemplo: SELECT * FROM person WHERE gender = 'Female';
+
+--Usando and para concatenar condiciones
+SELECT * FROM [TableName] WHERE [FieldName] = '[MyCondition]' AND [FieldName] = '[MyCondition2]';
+--Ejemplo SELECT * FROM person WHERE gender = 'Female' AND country_of_birth = 'Poland';
+
+--Usando la sentencia OR
+SELECT * FROM [TableName] WHERE [FieldName] = '[MyCondition]' AND ( [FieldName] = '[MyCondition2]'  OR  [FieldName] = '[MyCondition2?]' );
+--Ejemplo SELECT * FROM person WHERE gender = 'Female' AND (country_of_birth = 'Poland' OR country_of_birth = 'China');
+
+
+--Operadores de comparación y logicos
+SELECT 1 = 1; --Este regresaria
+--?column?
+--------------------
+--t    --Esto cambia dependiendo de si es verdadero o falso
+
+--SELECT 1 =  2;
+--SELECT 1 <  2;
+--SELECT 1 <= 2;
+--SELECT 1 <  1;
+--SELECT 'AMIGOSCODE' <> 'amigoscode';
+--SELECT 'AMIGOSCODE' = 'AMIGOSCODE';
+
+--Limit, offset & Fetch
+
+    --LIMIT
+    SELECT * FROM [TableName] LIMIT 10;
+        --Ejemplo: SELECT * FROM person LIMIT 5;
+
+    --OFFSET esto iniciaria a darte registros apartir del numero 6
+    SELECT * FROM [TableName] LIMIT 5 OFFSET 5;
+        --Ejemplo: SELECT * FROM person LIMIT 5 OFFSET 5;
